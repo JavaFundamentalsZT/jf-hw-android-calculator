1e6de1a2365f2d726bdb8f02c9002db9e5910d93
Madis Pink <madis.pink@gmail.com>
2016-11-29 02:20:56 +0200

    Remove HelpActivity

--- src/main/java/org/zeroturnaround/jf/android/calculator/CalculatorActivity.java
+++ src/main/java/org/zeroturnaround/jf/android/calculator/CalculatorActivity.java
@@ -1,11 +1,7 @@
 package org.zeroturnaround.jf.android.calculator;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 public class CalculatorActivity extends Activity {
     @Override
@@ -13,21 +9,5 @@ public class CalculatorActivity extends Activity {
         super.onCreate(savedInstanceState);
         // set our calculator layout
         setContentView(R.layout.main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // inflate our menu resource to have an in-app help / assignment description
-        new MenuInflater(this).inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menuHelp) {
-            startActivity(new Intent(this, HelpActivity.class));
-            return true;
-        }
-        return false;
     }
 }
--- src/main/java/org/zeroturnaround/jf/android/calculator/HelpActivity.java
+++
@@ -1,6 +0,0 @@
-package org.zeroturnaround.jf.android.calculator;
-
-import android.app.Activity;
-
-public class HelpActivity extends Activity {
-}
